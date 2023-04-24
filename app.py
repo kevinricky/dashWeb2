e891cf48744ad86d78b4c2a637124031b7b7dcba
kevinricky <79420928+kevinricky@users.noreply.github.com>
2023-04-24 06:58:16 +0530

    Read from blob

--- app.py
+++ app.py
@@ -1,20 +1,28 @@
 import dash
-import dash_core_components as dcc
-import dash_bootstrap_components as dbc  # pip install dash-bootstrap-components
 import dash_html_components as html
 from dash.dependencies import Input, Output, State
 import plotly.express as px
 
 import pandas as pd
+import csv
 
 from dash import dash_table
+
+from azure.storage.blob import BlobServiceClient
 
 # df = pd.read_csv('assets/data/gapminderDataFiveYear.csv')
 
 dash_app = dash.Dash(__name__)
 app = dash_app.server
 
-df = pd.read_csv('Hi.csv', header=0)
+# df = pd.read_csv('Hi.csv', header=0)
+connect_str = 'DefaultEndpointsProtocol=https;AccountName=dashstorageforkevin;AccountKey=A5qu9+GmBBhdwC3oz2s3ADYFMOaP4P7kQoWtuw9rDa533BFTDqGgy6XvEUcj2yeItBC9MPgZXTxB+AStsT4eXQ==;EndpointSuffix=core.windows.net'
+bsc = BlobServiceClient.from_connection_string(connect_str)
+blob_client_instance = bsc.get_blob_client('dashstorageforkevin', 'Hi.csv', snapshot=None)
+with open('/tmp/Temp_Hi.csv', "wb") as my_blob:
+    blob_data = blob_client_instance.download_blob()
+    blob_data.readinto(my_blob)
+df = pd.read_csv('/tmp/Temp_Hi.csv')
 
 dash_app.layout = html.Div([
     dash_table.DataTable(
@@ -27,6 +35,24 @@ dash_app.layout = html.Div([
         editable=True,
     ),
 ])
+
+
+def writeDataToBlob(df_temp):
+    # Create a blob client using the local file name as the name for the blob
+    bsc = BlobServiceClient.from_connection_string(connect_str)
+    cc = bsc.get_container_client('dashstorageforkevin')
+
+    fn = '/tmp/Hi.csv'
+    with open(fn, mode='w', newline='') as cf:
+        writer = csv.writer(cf)
+        writer.writerow(df_temp.columns.tolist())
+        for i in df_temp.shape[0]:
+            writer.writerow(df_temp.loc[i, :].values.tolist())
+    
+    bc = cc.get_blob_client(fn)
+    with open(fn, 'rb') as data:
+        bc.upload_blob(data)
+    return
 
 
 @dash_app.callback(
@@ -44,7 +70,8 @@ def update_columns(timestamp, rows):
         except:
             print('Error')
     df_temp = pd.DataFrame(data={'Name': Name, 'Age': Age})
-    df_temp.to_csv('Hi.csv')
+    # df_temp.to_csv('Hi.csv')
+    writeDataToBlob(df_temp)
     return rows
 
 
