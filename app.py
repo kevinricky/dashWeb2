558684c7a7e5c11d7b0b4c048ef56bca35eda91e
kevinricky <79420928+kevinricky@users.noreply.github.com>
2023-04-24 07:49:33 +0530

    Blob working

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ import plotly.express as px
 
 import pandas as pd
 import csv
+import os
+import time
 
 from dash import dash_table
 
@@ -18,11 +20,13 @@ app = dash_app.server
 # df = pd.read_csv('Hi.csv', header=0)
 connect_str = 'DefaultEndpointsProtocol=https;AccountName=dashstorageforkevin;AccountKey=A5qu9+GmBBhdwC3oz2s3ADYFMOaP4P7kQoWtuw9rDa533BFTDqGgy6XvEUcj2yeItBC9MPgZXTxB+AStsT4eXQ==;EndpointSuffix=core.windows.net'
 bsc = BlobServiceClient.from_connection_string(connect_str)
-blob_client_instance = bsc.get_blob_client('dashstorageforkevin', 'Hi.csv', snapshot=None)
-with open('/tmp/Temp_Hi.csv', "wb") as my_blob:
+blob_client_instance = bsc.get_blob_client('dashcontainersample', 'Hi.csv', snapshot=None)
+if not os.path.exists("tmp"):
+    os.mkdir('tmp')
+with open('tmp/Temp_Hi.csv', "wb") as my_blob:
     blob_data = blob_client_instance.download_blob()
     blob_data.readinto(my_blob)
-df = pd.read_csv('/tmp/Temp_Hi.csv')
+df = pd.read_csv('tmp/Temp_Hi.csv')
 
 dash_app.layout = html.Div([
     dash_table.DataTable(
@@ -40,17 +44,20 @@ dash_app.layout = html.Div([
 def writeDataToBlob(df_temp):
     # Create a blob client using the local file name as the name for the blob
     bsc = BlobServiceClient.from_connection_string(connect_str)
-    cc = bsc.get_container_client('dashstorageforkevin')
+    cc = bsc.get_container_client('dashcontainersample')
+    print('Writing data')
 
-    fn = '/tmp/Hi.csv'
+    fn = 'tmp/Hi.csv'
     with open(fn, mode='w', newline='') as cf:
         writer = csv.writer(cf)
         writer.writerow(df_temp.columns.tolist())
-        for i in df_temp.shape[0]:
+        for i in range(df_temp.shape[0]):
             writer.writerow(df_temp.loc[i, :].values.tolist())
-    
-    bc = cc.get_blob_client(fn)
+    print('Write data into container')
+    bc = bsc.get_blob_client('dashcontainersample', 'Hi.csv', snapshot=None)
     with open(fn, 'rb') as data:
+        # bc.rename_blob('Hi_'+str(time.time()+'.csv'))
+        bc.delete_blob()
         bc.upload_blob(data)
     return
 
